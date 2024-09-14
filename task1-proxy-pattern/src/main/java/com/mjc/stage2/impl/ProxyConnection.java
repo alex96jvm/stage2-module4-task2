d3848d8f8af9831bdc7646d9ba8522c053d0153b
alex96jvm <alex96jvm@gmail.com>
2024-09-14 13:16:55 +0300

    Use releaseConnection

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -4,27 +4,24 @@ import com.mjc.stage2.Connection;
 
 public class ProxyConnection implements Connection {
     private RealConnection realConnection;
-    private boolean isClosed;
 
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
-        this.isClosed = false;
     }
 
     public void reallyClose() {
-        if (!isClosed && !realConnection.isClosed()) {
-            realConnection.close();
-            isClosed = true;
+        realConnection.close();
         }
-    }
 
     @Override
     public void close() {
-
+        ConnectionPool cp = ConnectionPool.getInstance();
+        cp.releaseConnection(this);
     }
 
     @Override
     public boolean isClosed() {
-        return isClosed || realConnection.isClosed();
+        return realConnection.isClosed();
     }
 }
+
