fd6d2a71aa945805f09115e0197d2405c100d8e0
alex96jvm <alex96jvm@gmail.com>
2024-09-14 11:59:23 +0300

    Add state to ProxyConnection

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -4,26 +4,27 @@ import com.mjc.stage2.Connection;
 
 public class ProxyConnection implements Connection {
     private RealConnection realConnection;
+    private boolean isClosed;
 
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
+        this.isClosed = false;
     }
 
     public void reallyClose() {
-        if (!realConnection.isClosed()) {
+        if (!isClosed && !realConnection.isClosed()) {
             realConnection.close();
+            isClosed = true;
         }
     }
 
     @Override
     public void close() {
-        if (!realConnection.isClosed()) {
-            realConnection.close();
-        }
+
     }
 
     @Override
     public boolean isClosed() {
-        return realConnection.isClosed();
+        return isClosed || realConnection.isClosed();
     }
 }
